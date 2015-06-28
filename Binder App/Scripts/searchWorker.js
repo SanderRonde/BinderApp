aa917599a4b435fe38359eecc1e77f879ea496d2
Sander Ronde <awsdfgvhbjn@gmail.com>
2015-06-28 09:38:51 +0200

    Searchworker can now be cancelled, making it stop searching for unnessecary data

--- Binder App/Scripts/searchWorker.js
+++ Binder App/Scripts/searchWorker.js
@@ -1,16 +1,30 @@
-function searchData(query, data) {
-	var matchingData = [];
-	var currentData;
-	for (var i = 0; i < data.length; i++) {
-		currentData = data[i];
-		if (currentData.name.indexOf(query) > -1) {
-			matchingData.push(currentData);
+var currentSearch = null;
+
+function SearchQuery(query, data) {
+	this.active = true;
+	var thisFunction = this;
+
+	this.search = function() {
+		var matchingData = [];
+		var currentData;
+		for (var i = 0; i < data.length && thisFunction.active; i++) {
+			currentData = data[i];
+			if (currentData.name.indexOf(query) > -1) {
+				matchingData.push(currentData);
+			}
 		}
+		postMessage(matchingData);
+	}();
+
+	this.stop = function() {
+		this.active = false;
 	}
-	postMessage(matchingData);
 }
 
 self.addEventListener('message', function(e) {
 	var data = e.data;
-	searchData(data.search, data.searchEngines);
+	if (currentSearch !== null) {
+		currentSearch.stop();
+	}
+	currentSearch = new SearchQuery(data.search, data.data);
 }, false);

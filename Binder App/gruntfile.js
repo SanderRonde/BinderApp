cfc9fb84183f6b5a490d441d1d6cffb87379d2e9
Sander Ronde <awsdfgvhbjn@gmail.com>
2015-06-30 21:24:22 +0200

    Now uses a preprocessor for building, see other commits for details. Added a dev task without minification (on the js/css that is)

--- Binder App/gruntfile.js
+++ Binder App/gruntfile.js
@@ -1,17 +1,6 @@
 ﻿module.exports= function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		htmlmin: {
-			dist: {
-				options: {
-					removeComments: false,
-					collapseWhitespace: true
-				},
-				files: {
-					'build/window.html': 'window.html'
-				}
-			}
-		},
 		minified: {
 			files: {
 				src: [
@@ -39,11 +28,21 @@
 		copy: {
 			main: {
 				files: [
-					{expand: true, src: ['Scripts/jquery-2.0.3.min.js', 'Scripts/jquery.color-2.1.2.min.js', 'jquery.easing.js'], dest: 'build/'},
-					{expand: true, src: ['Images/*'], dest: 'build/'},
-					{expand: true, src: ['Colorpicker/**'], dest: 'build/' },
-					{expand: true, src: ['LICENSE.txt', 'manifest.json'], dest: 'build/' },
-					{expand: true, src: ['icon-large.png', 'icon-small.png', 'icon-supersmall.png', 'manifest.json', 'Segoe_UI.ttf'], dest: 'build/'}
+					{ expand: true, src: ['Scripts/jquery-2.0.3.min.js', 'Scripts/jquery.color-2.1.2.min.js', 'jquery.easing.js'], dest: 'build/' },
+					{ expand: true, src: ['Images/*'], dest: 'build/' },
+					{ expand: true, src: ['Colorpicker/**'], dest: 'build/' },
+					{ expand: true, src: ['LICENSE.txt', 'manifest.json'], dest: 'build/' },
+					{ expand: true, src: ['icon-large.png', 'icon-small.png', 'icon-supersmall.png', 'manifest.json', 'Segoe_UI.ttf'], dest: 'build/' }
+				]
+			},
+			dev: {
+				files: [
+					{ expand: true, src: ['html/*'], dest: 'build/' },
+					{ expand: true, src: ['Scripts/*'], dest: 'build/' },
+					{ expand: true, src: ['Images/*'], dest: 'build/' },
+					{ expand: true, src: ['Colorpicker/**'], dest: 'build/' },
+					{ expand: true, src: ['LICENSE.txt', 'manifest.json'], dest: 'build/' },
+					{ expand: true, src: ['css.css','icon-large.png', 'icon-small.png', 'icon-supersmall.png', 'manifest.json', 'Segoe_UI.ttf'], dest: 'build/' }
 				]
 			}
 		},
@@ -51,7 +50,7 @@
 			jsCssBanner: {
 				options: {
 					position: 'top',
-					banner: '/*Original can be found at https://github.com/BarryBamibal/BinderApp */',
+					banner: '/* Original can be found at https://github.com/BarryBamibal/BinderApp \n * This code may only be used under the MIT style license found at in the LICENSE.txt file \n**/',
 					linebreak: true
 				},
 				files: {
@@ -61,12 +60,60 @@
 			htmlBanner: {
 				options: {
 					position: 'top',
-					banner: '<!--Original can be found at https://github.com/BarryBamibal/BinderApp*/-->',
+					banner: '<!--Original can be found at https://github.com/BarryBamibal/BinderApp\nThis code may only be used under the MIT style license found at in the LICENSE.txt file-->\n',
 					linebreak: true
 				},
 				files: {
-					src: ['build/window.html']
+					src: ['build/html/*']
 				}
+			}
+		},
+		processhtml: {
+			with: {
+				files: {
+					'build/html/withSuperSearch.html': ['html/window.html']
+				}
+			},
+			without: {
+				files: {
+					'build/html/withoutSuperSearch.html': ['html/window.html']
+				}
+			},
+			black: {
+				options: {
+					strip: true,
+				},
+				files: [
+					{ expand: true, src: ['build/html/withoutSuperSearch.html', 'build/html/withSuperSearch.html'], filter: 'isFile', ext: '.black.html' }
+				]
+			},
+			red: {
+				options: {
+					strip: true,
+				},
+				files: [
+					{ expand: true, src: ['build/html/withoutSuperSearch.html', 'build/html/withSuperSearch.html'], filter: 'isFile', ext: '.red.html' }
+				]
+			},
+			white: {
+				options: {
+					strip: true,
+				},
+				files: [
+					{ expand: true, src: ['build/html/withoutSuperSearch.html', 'build/html/withSuperSearch.html'], filter: 'isFile', ext: '.white.html' }
+				]
+			}
+		},
+		htmlmin: {
+			dist: {
+				options: {
+					removeComments: false,
+					collapseWhitespace: true,
+					minifyCSS: true
+				},
+				files: [
+					{ expand: true, src: ['build/html/*'], filter: 'isFile' }
+				]
 			}
 		},
 		zip: {
@@ -75,7 +122,7 @@
 				src: ['build/**'],
 				dest: 'build/Binder App.zip'
 			}
-		}
+		},
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-copy');
@@ -84,6 +131,8 @@
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-banner');
 	grunt.loadNpmTasks('grunt-zip');
+	grunt.loadNpmTasks('grunt-processhtml');
 
-	grunt.registerTask('build', ['htmlmin', 'minified','cssmin','copy','usebanner','zip']);
+	grunt.registerTask('build', ['minified', 'cssmin', 'copy:main', 'processhtml', 'usebanner', 'htmlmin', 'zip']);
+	grunt.registerTask('dev', ['copy:dev', 'processhtml', 'htmlmin']);
 }
